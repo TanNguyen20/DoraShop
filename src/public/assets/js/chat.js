e266110acd5a4bb15a9f1ec781e36345b37d6202
Nguyen Nhat Tan <nguyennhattan1562000@gmail.com>
2021-06-29 21:51:59 +0700

    modify layout socket

--- src/public/assets/js/chat.js
+++ src/public/assets/js/chat.js
@@ -5,12 +5,12 @@ $(function () {
     //Socket nhận data và append vào giao diện
     socket.on("send", function (data) {
         console.log(data);
-        $("#content").append("<p class='message__container'>" + data.username + ": "+"<span class='content-message'>" + data.message + "</span>"+"</p>")
+        $("#content-socket").append("<p class='message__container'>" + data.username + ": "+"<span class='content-message'>" + data.message + "</span>"+"</p>")
     })
 
     //Bắt sự kiện click gửi message
     $("#sendMessage").on('click', function () {
-        var username = $('#username').val();
+        var username = $('#username-socket').val();
         var message = $('#message').val();
 
         if (username == '' || message == '') {
@@ -21,4 +21,19 @@ $(function () {
             $('#message').val('');
         }
     })
+    $("#message").on("keyup",function(event) {
+        if(event.which===13){
+            var username = $('#username-socket').val();
+            var message = $('#message').val();
+    
+            if (username == '' || message == '') {
+                alert('Please enter name and message!!');
+            } else {
+                //Gửi dữ liệu cho socket
+                socket.emit('send', {username: username, message: message});
+                $('#message').val('');
+            }
+        }
+       
+    });
 })
